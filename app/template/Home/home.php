d9c0754472342279dd3f8d7bfaade8a95654a03a
Yagamii <yagogomesc@gmail.com>
2016-06-22 01:58:17 -0300

    Adicionado link ao nome, imagem e botão ver

--- app/template/Home/home.php
+++ app/template/Home/home.php
@@ -3,10 +3,10 @@
 	//Verifica cada produto que foi chamado na variavel $produtos que veio do act_home para exibi-lo na tela
 	while($row = mysqli_fetch_array($produtos, MYSQLI_ASSOC)): ?>
 	<div class="produto-curto">
-    	<h3><?php echo $row['nome_produto']; ?></h3>
-    	<img src="app/template/Includes/thumb/<?php echo $row['thumb']; ?>" width="220" height="210"/>
+    	<h3><a href="index.php?page=produto&id=<?php echo $row['id_produto']; ?>" ><?php echo $row['nome_produto']; ?></a></h3>
+    	<a href="index.php?page=produto&id=<?php echo $row['id_produto']; ?>" ><img src="app/template/Includes/thumb/<?php echo $row['thumb']; ?>" width="220" height="210"/></a>
         <p class="desc-produto"><?php echo $row['descricao']; ?> </p>
         <div class="valor">R$<?php echo $row['valor']; ?></div>
-        <div class="ver-produto">Ver</div>
+        <a href="index.php?page=produto&id=<?php echo $row['id_produto']; ?>" ><div class="ver-produto">Ver</div></a>
 	</div>
 <?php endwhile; ?>
