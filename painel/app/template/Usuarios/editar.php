9cea1ac4ede468e143ae1cdf67c31b6cf067a869
Yagamii <yagogomesc@gmail.com>
2016-07-25 18:24:36 -0300

    Ajustado posição dos campos

--- painel/app/template/Usuarios/editar.php
+++ painel/app/template/Usuarios/editar.php
@@ -4,10 +4,10 @@
         <form action="index.php?page=usuarios&action=editar&id=<?php echo $_GET['id'];?>" name="editUsuario" method="post">
         <br/>
         <label>Nome:</label><input type="text" name="nome" value="<?php echo $_infos['nome']; ?>" /><br/><br/>
-        <label>Sobrenome:</label><input type="text" name="sobrenome" value="<?php echo $_infos['sobrenome']; ?>" /><br/><br/>
+        <label style="margin-left: -32px">Sobrenome:</label><input type="text" name="sobrenome" value="<?php echo $_infos['sobrenome']; ?>" /><br/><br/>
         <label>Email:</label><input type="text" name="email" value="<?php echo $_infos['email']; ?>" /><br/><br/>
-        <label>Usuário:</label><input type="text" name="usuario" value="<?php echo $_infos['usuario']; ?>" /><br/><br/>
-        <label>Função:</label><select name="nivel">
+        <label style="margin-left: -10px">Usuário:</label><input type="text" name="usuario" value="<?php echo $_infos['usuario']; ?>" /><br/><br/>
+        <label style="margin-left: -78px">Função:</label><select name="nivel">
         						<option></option>
         						<?php while($row = mysqli_fetch_array($nivel, MYSQLI_ASSOC)): 
                                 	if($_infos['id_nivel'] == $row['id_nivel']){
